b0d3e8b8ca73d3541ff11000cdd026f4d063b29e
Eyüp Can Esen <eyupcanee@gmail.com>
2023-03-31 19:15:29 +0300

    getAdminCached function has coded

--- server/tests/AdminCache.js
+++ server/tests/AdminCache.js
@@ -10,4 +10,23 @@ let redisClient;
   await redisClient.connect();
 })();
 
-export const getAdminCached = async (req, res, next) => {};
+export const getAdminCached = async (req, res, next) => {
+  const { id } = req.params;
+  let results;
+
+  try {
+    const cacheResults = await redisClient.get(id);
+    if (cacheResults) {
+      results = JSON.parse(cacheResults);
+      res.status(200).json({
+        status: "ok",
+        fromCache: true,
+        data: results,
+      });
+    } else {
+      next();
+    }
+  } catch (error) {
+    res.status(404).json({ status: "no" });
+  }
+};
